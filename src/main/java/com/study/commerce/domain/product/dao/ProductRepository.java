ca61a5cf8761b5c2e904c88b511b6954604e80a5
daseul-jang <devzzang94@gmail.com>
2024-06-25 02:41:16 +0900

    feat: 제품 repository 생성

---
+++ src/main/java/com/study/commerce/domain/product/dao/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.study.commerce.domain.product.dao;
+
+import com.study.commerce.domain.product.entity.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+}
