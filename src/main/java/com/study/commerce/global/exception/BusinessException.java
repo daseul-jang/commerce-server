4a1e57716a5b9335d64d990c1fb21eb78c6daeb2
daseul-jang <devzzang94@gmail.com>
2024-06-25 02:38:53 +0900

    feat: 예외 처리 설정

---
+++ src/main/java/com/study/commerce/global/exception/BusinessException.java
@@ -0,0 +1,19 @@
+package com.study.commerce.global.exception;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class BusinessException extends RuntimeException {
+    private HttpStatus status;
+    private String message;
+
+    public BusinessException(HttpStatus status, String message) {
+        super(message);
+        this.status = status;
+        this.message = message;
+    }
+}
